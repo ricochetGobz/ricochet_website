28383054925cab421c935533eb06a8a28b7a7a4d
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-06-19 11:13:05 +0200

    :ambulance: fix router and webpack to build on production

--- app/router.js
+++ app/router.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHistory } from 'history';
 
 import Layout from './views/Layout';
 import Home from './views/Home/Home';
 import About from './views/About/About';
 
+
+const history = useRouterHistory(createHistory)({
+  basename: window.location.pathname,
+});
+
 export default function Root() {
   return (
-    <Router history={browserHistory}>
+    <Router history={history}>
       <Route path="/" component={Layout} >
           <IndexRoute component={Home} />
           <Route path="/apropo" component={About} />
